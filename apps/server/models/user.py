eedd96aa20c0f79d2309065b80ba9a0b4272fedb
Ashley2n <a.windbreacker@gmail.com>
2026-05-04 11:05:35 -0500

    Push any updates

--- apps/server/models/user.py
+++ apps/server/models/user.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+from pyexpat.errors import messages
 from sqlalchemy import Column, Integer, String, DateTime, Sequence, func
 from sqlalchemy.dialects.postgresql.base import UUID
 
@@ -10,11 +11,12 @@ from apps.server.core.database import Base
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String,nullable=False)
-    email = Column(String, unique=True, nullable=True)
-    password = Column(String, nullable=False)
-    profile_images_url = Column(String, nullable=True, default='default_profile_image.png')
-    bio = Column(String)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    username = Column(String(30),nullable=False, unique=True, index=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    password_hash = Column(String(255), nullable=False)
+
+    profile_images_url = Column(String(500), nullable=True, default='default_profile_image.png')
+    bio = Column(String(160), nullable=True, default='')
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
